3fd52643687e51766539910d326ac7df4c1bc5dd
大野颯一郎 <soichrooono@gmail.com>
2020-09-30 21:48:14 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+#models.pyからPostモデルを読み込んで、こっちでも使えるようにする
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	#lte は less than or equal to で現在時刻より前に作られたデータを抽出
+	#公開時刻順で、Postモデルのobjectsをpost変数に代入。これでモデル → ビュー のデータ渡しはできた
+	return render(request, 'blog/post_list.html', {'posts': posts})
+    #"posts"という名前で、post変数を html(テンプレート)側へ渡す
